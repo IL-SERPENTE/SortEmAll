55a94349e7051fc510ebe7e15756faa2da83933a
BlueSlime <jeremylevilain78@orange.fr>
2017-08-24 15:18:52 +0200

    Publishing source code.

--- src/main/java/com/github/keyzou/villagerrun/tasks/GameTask.java
+++ src/main/java/com/github/keyzou/villagerrun/tasks/GameTask.java
@@ -6,6 +6,22 @@ import net.samagames.tools.GameUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
+/*
+ * This file is part of PersistanceAPI.
+ *
+ * PersistanceAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PersistanceAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PersistanceAPI.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class GameTask extends BukkitRunnable {
 
     private VillagerRun game;
